eff79059e411f7f433f72d890304867f169a6b85
GrimbiXcode <grimbixcode@gmail.com>
2017-02-24 10:18:33 +0100

    finishing the live-chart-script
    cpu-live-data.js script
    - clear commenting of the cpu-live-data.js script
    - bug-fix: send more than one datapoint
    - performance: timeout convertet to interval
    - cleanup redundant code
    General
    - cleanup js-files in static/data/ directory

--- simpleSite/processor/static/data/cpu-live-data.js
+++ simpleSite/processor/static/data/cpu-live-data.js
@@ -1,14 +1,26 @@
-var cpuUssage = [];
-var cpuUssagePointer = 0; // pointer for redrawing
-var dataset;
-var totalPoints = 100;
-var plotcut = totalPoints*0.04; // define distance between old plot and cpuUssagePointer
-var updateInterval = 500;
-var data = [];
-var now = 0;
-var index = 0;
-var samplesPerRendering = 2;
+/*===============================================================================
+# File:         cpu-live-data.js
+# Author:       David Grimbichler
+# Date:         17.01.2017
+# License:      GNU GENERAL PUBLIC LICENSE V3
+# =============================================================================*/
 
+
+/*===============================================================================
+# GLOBAL VARIABLES
+===============================================================================*/
+var cpuUssage = [];             // Array to draw the graph
+var cpuUssagePointer = 0;       // pointer for redrawing
+var dataset;                    // variable for collection with datapoints and graphsettings used by flot
+var totalPoints = 100;          // define the length of x-Axis (amount of datapoints)
+var plotcut = totalPoints*0.1;  // define distance between old plot and cpuUssagePointer
+var chartUpdateInterval = 20;   // update interval in ms (lower = smoother but more cpu-ussage)
+var getDataInterval = 500;      // timeout interval to get new data in ms
+var data = [];                  // Array for received datapoints
+var samplesPerRendering = 2;    // set it to min 1+datapoint received per update interval to prevent 
+                                    // getting data garbage in the "data"-Array
+
+/* Settings/options for the chart */
 var options = {
     series: {
         lines: {
@@ -75,80 +87,146 @@ var options = {
     },
 };
 
-
-/* Function for value in table */
-function UpdateTableValue(xml) {
-    var xmlDoc = xml.responseXML;
-    var newcpuUssage = xmlDoc.getElementsByTagName("CPU_USSAGE")[0].childNodes[0].nodeValue
-    document.getElementById('cpu-ussage').innerHTML = newcpuUssage;
-    data.push(newcpuUssage);
-    setTimeout(GetData, updateInterval);
-}
+/*-------------------------------------------------------------------------------
+# END OF SECTION GLOBAL VARIABLE
+-------------------------------------------------------------------------------*/
 
 
+/*===============================================================================
+# FUNCTIONS
+===============================================================================*/
+
+/********************************************************************************
+* GetData()
+* ------------------------------------------------------------------------------
+* send a request to the server to get data
+* and fetch received data into the receive buffer
+********************************************************************************/
 function GetData() {
-  var xhttp;
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      UpdateTableValue(this);
-    }
-  };
-  xhttp.open("POST", "serverussage", true);
-  xhttp.send();   
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+
+    /* define the callback function, executed by received data from server */
+    xhttp.onreadystatechange = function() {
+        /* check if everything is ready */
+        if (this.readyState == 4 && this.status == 200) {
+            /* read the XML-formatet data */
+            var xmlDoc = this.responseXML;
+            /* fetch the XML data*/
+            var newcpuUssage;
+            for (var i = 0; xmlDoc.getElementsByTagName("CPU_USSAGE")[0].childNodes.length > i; i++) {
+                newcpuUssage = xmlDoc.getElementsByTagName("CPU_USSAGE")[0].childNodes[i].nodeValue;
+                /* put it into the buffer */
+                data.push(newcpuUssage);
+                }
+            /* write the last value into the placeholder "cpu-ussage" */
+            document.getElementById('cpu-ussage').innerHTML = newcpuUssage;
+        }
+    }; // end of callback function
+
+    /* send a POST-request to the server  with the URL: "http://yourserver/serverussage" */
+    xhttp.open("POST", "serverussage", true);
+    xhttp.send();   
 }
 
-function update(_data) {
-    for (var i = 0; i < _data.length; i += 1) {
-        data.push(_data[i]);
+/********************************************************************************
+* End of GetData()
+********************************************************************************/
+
+/********************************************************************************
+* updateChart()
+* ------------------------------------------------------------------------------
+* Engine to redraw the chart every "chartUpdateInterval"
+********************************************************************************/
+function updateChart() {
+    /* are new data received? */
+    if (!(data.length)){
+        return;
     }
-    
-    setTimeout(GetData, updateInterval);
+
+    /* render the received data */
+    getRendered();
+
+    /* plot the chart with the dataset and the options */
+    $.plot($("#flot-live-chart"), dataset, options);
 }
 
+/********************************************************************************
+* End of updateChart()
+********************************************************************************/
+
+/********************************************************************************
+* getRendered()
+* ------------------------------------------------------------------------------
+* render the received data
+********************************************************************************/
 function getRendered() {
     for (var i = 0; i < samplesPerRendering; i++){
         if (data.length) {
-                for (var j = 1; j < plotcut; j++){
-                    cpuUssage[cpuUssagePointer + j] = null;
+            /* cut the old plot for a better view of the new data*/
+            for (var j = 1; j < plotcut; j++){
+                cpuUssage[cpuUssagePointer + j] = null;
             }
+            /* put in the new data */
             cpuUssage[cpuUssagePointer] = [cpuUssagePointer, data[0]];
             cpuUssagePointer++;
-            cpuUssagePointer %= totalPoints;
+            cpuUssagePointer %= (totalPoints + 1);
+            /* and handle the receice buffer */
             data = data.slice(1);
         }
     }
 }
 
-function initData() {
+/********************************************************************************
+* End of getRendered()
+********************************************************************************/
 
-    if (data.length) {
-            data = data.slice(1);
-        }
-
+/********************************************************************************
+* initChartData()
+* ------------------------------------------------------------------------------
+* generate initial data to display a empty chart 
+********************************************************************************/
+function initChartData() {
+    /* fill empty data into the chart-data */
     for (var i = 0; i < totalPoints; i++) {
         cpuUssage.push([i,null]);
     }
 }
 
+/********************************************************************************
+* End of initChartData()
+********************************************************************************/
+/*-------------------------------------------------------------------------------
+# END OF SECTION FUNCTIONS
+-------------------------------------------------------------------------------*/
+
+
+/*===============================================================================
+# "MAIN"-CODE
+===============================================================================*/
+
+/* if Browser is ready: Run following function */
 $(document).ready(function () {
-    initData();
+    /* generate initial data to display a empty chart */
+    initChartData();
+
+    /* Generate the dataset for the chart-data */
     dataset = [
-        { label: "cpuUssage Live-Stream", data: cpuUssage, lines: { fill: false, lineWidth: 1.2 }, color: "#00FF00" }   
+        { label: "CPU Ussage [%] Live-Stream", data: cpuUssage, lines: { fill: false, lineWidth: 1.2 }, color: "#00FF00" }   
     ];
+
+    /* plot the chart with the generatet dataset and the options 
+    * use it here a first time to prevent a delay by displaying the chart
+    */
     $.plot($("#flot-live-chart"), dataset, options);
     
-    setTimeout(GetData, updateInterval);
-
+    /* start getting the data */
+    setInterval(GetData, getDataInterval);
+    /* start printing graphs*/
+    setInterval(updateChart, chartUpdateInterval);
 });
 
-setInterval(function updateData() {
-    if (!(data.length)){
-        return;
-    }
-        getRendered();
-    dataset = [
-        { label: "CPU Ussage [%] Live-Stream", data: cpuUssage, lines: { fill: false, lineWidth: 1.2 }, color: "#00FF00" }    
-    ];
-    $.plot($("#flot-live-chart"), dataset, options);
-}, 5);
+
+/*-------------------------------------------------------------------------------
+# END OF SECTION "MAIN"-CODE
+-------------------------------------------------------------------------------*/
